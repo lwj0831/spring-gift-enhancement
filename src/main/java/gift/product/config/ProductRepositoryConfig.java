b2b067a9a31bc9a788002643e5578fa8dad65631
lwj0831 <wjl0831@gmail.com>
2025-07-15 23:34:28 +0900

    chore(config): ProductRepositoryConfig 업데이트
    - dev,test 프로파일에서 JpaProductRepository구현체 사용하도록 변경

--- src/main/java/gift/product/config/ProductRepositoryConfig.java
+++ src/main/java/gift/product/config/ProductRepositoryConfig.java
@@ -1,9 +1,9 @@
 package gift.product.config;
 
 import gift.product.repository.InMemoryProductRepository;
-import gift.product.repository.JdbcProductRepository;
+import gift.product.repository.JpaProductRepository;
+import gift.product.repository.JpaProductRepositoryAdapter;
 import gift.product.repository.ProductRepository;
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,14 +12,14 @@ import org.springframework.context.annotation.Profile;
 public class ProductRepositoryConfig {
 
   @Bean
-  @Profile("dev")
-  public ProductRepository jdbcProductRepository(DataSource dataSource) {
-    return new JdbcProductRepository(dataSource);
+  @Profile({"test", "dev"})
+  public ProductRepository jpaProductRepositoryBean(JpaProductRepository jpaProductRepository) {
+    return new JpaProductRepositoryAdapter(jpaProductRepository);
   }
 
   @Bean
-  @Profile({"default", "test"})
-  public ProductRepository inMemoryProductRepository() {
+  @Profile("default")
+  public ProductRepository InMemoryProductRepositoryBean() {
     return new InMemoryProductRepository();
   }
 }
