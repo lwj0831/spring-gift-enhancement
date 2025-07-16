2ee76da13c4b074def0252ce53770e398c8b6974
lwj0831 <wjl0831@gmail.com>
2025-07-16 14:05:27 +0900

    feat(member): MemberEntity 정의 및 MemberMapper헬퍼 클래스 정의
    - 회원 엔티티 클래스 MemberEntity 정의
    - Member - MemberEntity 매핑 헬퍼 클래스 MemberMapper 정의

---
+++ src/main/java/gift/member/jpa/MemberEntity.java
@@ -0,0 +1,39 @@
+package gift.member.jpa;
+
+import gift.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="member")
+public class MemberEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  public MemberEntity() {
+  }
+
+  public MemberEntity(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public void updateFromDomain(Member member){
+    this.id = member.id();
+    this.name = member.name();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
---
+++ src/main/java/gift/member/jpa/MemberMapper.java
@@ -0,0 +1,15 @@
+package gift.member.jpa;
+
+import gift.member.domain.Member;
+
+public class MemberMapper {
+
+  public static Member toDomain(MemberEntity entity){
+    return Member.withId(entity.getId(),entity.getName());
+  }
+
+  public static MemberEntity toEntity(Member entity){
+    return new MemberEntity(entity.id(),entity.name());
+  }
+
+}
