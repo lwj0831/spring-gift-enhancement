8e977bd281a9175a8bef2c6396a3738e2246fccf
lwj0831 <wjl0831@gmail.com>
2025-07-16 16:29:55 +0900

    feat(wishItem): WishItem엔티티 클래스 및 WishITemMapper 헬퍼 클래스 도입

---
+++ src/main/java/gift/wishlist/jpa/WishItemEntity.java
@@ -0,0 +1,50 @@
+package gift.wishlist.jpa;
+
+import gift.member.jpa.MemberEntity;
+import gift.product.jpa.ProductEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "wish_item")
+public class WishItemEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private MemberEntity member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id")
+  private ProductEntity product;
+
+  public WishItemEntity() {
+  }
+
+  public WishItemEntity(Long id, MemberEntity member, ProductEntity product) {
+    this.id = id;
+    this.member = member;
+    this.product = product;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public MemberEntity getMember() {
+    return member;
+  }
+
+  public ProductEntity getProduct() {
+    return product;
+  }
+}
