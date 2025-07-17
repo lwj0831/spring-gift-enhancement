baad211a92319d87316a85b2ec86f06016eec158
lwj0831 <wjl0831@gmail.com>
2025-07-17 20:54:37 +0900

    chore: 불필요한 ActiveProfiles 제거

--- src/test/java/gift/wishlist/repository/WishItemJpaRepositoryTest.java
+++ src/test/java/gift/wishlist/repository/WishItemJpaRepositoryTest.java
@@ -13,11 +13,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @DisplayName("WishItemJpaRepository 테스트")
-@ActiveProfiles("test")
 class WishItemJpaRepositoryTest {
 
     @Autowired
