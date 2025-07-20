2edd55d24b9d268a57a3b3f733e2beec2171986d
lwj0831 <wjl0831@gmail.com>
2025-07-20 22:04:22 +0900

    chore: data-default.sql 더미데이터 추가

--- src/main/resources/data-default.sql
+++ src/main/resources/data-default.sql
@@ -1,26 +1,48 @@
-INSERT INTO product (name, price, description, image_url)
-VALUES ('테스트 상품1', 1000, '설명1', 'https://img1.url');
-INSERT INTO product (name, price, description, image_url)
-VALUES ('테스트 상품2', 2000, '설명2', 'https://img2.url');
 INSERT INTO product (id, name, price, description, image_url)
-VALUES (100, '테스트 상품3', 2000, '설명2', 'https://img2.url');
+VALUES (100, '테스트 상품0', 100, '설명1', 'https://img.url');
 INSERT INTO product (id, name, price, description, image_url)
-VALUES (101, '테스트 상품4', 2000, '설명2', 'https://img2.url');
+VALUES (101, '테스트 상품1', 1000, '설명2', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (102, '테스트 상품2', 2000, '설명2', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (103, '테스트 상품3', 3000, '설명3', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (104, '테스트 상품4', 4000, '설명4', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (105, '테스트 상품5', 5000, '설명5', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (106, '테스트 상품6', 6000, '설명6', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (107, '테스트 상품7', 7000, '설명7', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (108, '테스트 상품8', 8000, '설명8', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (109, '테스트 상품9', 9000, '설명9', 'https://img.url');
+INSERT INTO product (id, name, price, description, image_url)
+VALUES (110, '테스트 상품10', 10000, '설명10', 'https://img.url');
 
--- INSERT INTO member (product_id, name)
--- VALUES (100, 'kim');
--- INSERT INTO member (product_id, name)
--- VALUES (101, 'park');
---
--- INSERT INTO member_auth (id, email, password, refresh_token)
--- VALUES (100, 'wjl0831@gmail.com', '$2a$10$C9fK7K1QZUXmCqZsMR8VdupxCBFe2g7kWyWeLb9bPP1WjAzBtHSSK',
---         'abcd');
--- INSERT INTO member_auth (id, email, password, refresh_token)
--- VALUES (200, 'park@example.com', '$2a$10$7jxHkc3x2P8ZbmHcDxOXke95hWuHx6cLG3axrGgVqLEK3KLS/ZUQ6',
---         'abcd');
---
--- INSERT INTO wish_item (member_id, product_id)
--- VALUES (100, 100);
--- INSERT INTO wish_item (member_id, product_id)
--- VALUES (100, 101);
+
+INSERT INTO member (id, name)
+VALUES (100, 'lee');
+
+
+
+INSERT INTO member_auth (id, email, password, refresh_token)
+VALUES (100, 'wjl0831@gmail.com', '$2a$10$JunqFWsxQbDLzmNAAgXNKuKW77ehhv9cuozq75SkkbJZEYtee1Zm.',
+        'abcd');
+
+INSERT INTO wish_item (member_id, product_id)
+VALUES (100, 100);
+INSERT INTO wish_item (member_id, product_id)
+VALUES (100, 101);
+INSERT INTO wish_item (member_id, product_id)
+VALUES (100, 103);
+INSERT INTO wish_item (member_id, product_id)
+VALUES (100, 105);
+INSERT INTO wish_item (member_id, product_id)
+VALUES (100, 107);
+INSERT INTO wish_item (member_id, product_id)
+VALUES (100, 109);
+INSERT INTO wish_item (member_id, product_id)
+VALUES (100, 110);
 
