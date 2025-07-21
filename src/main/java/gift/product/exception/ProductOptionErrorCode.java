427409c1b9b7d17c4e274006599b83821d67e5d7
lwj0831 <wjl0831@gmail.com>
2025-07-21 23:02:34 +0900

    feat(product): ProductOption관련 예외 정의 및 핸들러 구현

--- src/main/java/gift/product/exception/ProductOptionErrorCode.java
+++ src/main/java/gift/product/exception/ProductOptionErrorCode.java
@@ -3,15 +3,18 @@ package gift.product.exception;
 import gift.global.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
-public enum ProductErrorCode implements ErrorCode {
-    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT-001", "상품 정보를 찾을 수 없습니다"),
-    INVALID_PRODUCT_NAME(HttpStatus.BAD_REQUEST, "PRODUCT-002", "상품명에 포함할 수 없는 키워드가 존재합니다"),
-    INVALID_SORT_FIELD_ERROR(HttpStatus.BAD_REQUEST, "PRODUCT-003", "상품 페이징 정렬 필드 값이 올바르지 않습니다");
+public enum ProductOptionErrorCode implements ErrorCode {
+
+    DUPLICATE_PRODUCT_OPTION_NAME(HttpStatus.BAD_REQUEST, "PRODUCT-004",
+        "해당 상품에 동일한 옵션명의 옵션이 존재합니다."),
+    PRODUCT_OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "PRODUCT-005", "상품 옵션이 존재하지 않습니다."),
+    INVALID_PRODUCT_OPTION_NAME(HttpStatus.BAD_REQUEST, "PRODUCT-006", "유효하지 않은 상품 옵션명입니다.");
+
     private final HttpStatus status;
     private final String errorCode;
     private final String errorMessage;
 
-    ProductErrorCode(HttpStatus status, String errorCode, String errorMessage) {
+    ProductOptionErrorCode(HttpStatus status, String errorCode, String errorMessage) {
         this.status = status;
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
